e472d7ded66399f47538146fd6e7d71a9d58f082
Laszlo Suslecz <drsuslac@gmail.com>
2017-09-15 16:08:36 +0200

    feat: implement basic routing and add basic folder structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import MainSection from './components/MainSection'
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Route exact path='/' render={() => (
+          <MainSection />
+        )}/>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ frontend/src/components/MainSection.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class MainSection extends Component {
+  render() {
+    return ( 
+      <h1>This is the main page</h1>
+    )
+  }
+}
+
+export default MainSection
