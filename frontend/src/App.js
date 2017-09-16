341b9d69b29926c0456d328feb06b998c0740bdc
Laszlo Suslecz <drsuslac@gmail.com>
2017-09-16 15:54:50 +0200

    feat: add 404 page and <Switch> to routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import MainSection from './components/MainSection'
 import CategoryView from './components/CategoryView'
 import PostDetail from './components/PostDetail'
 import PostNew from './components/PostNew'
 import PostEdit from './components/PostEdit'
+import NoMatch from './components/NoMatch'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Route exact path='/' render={() => (
-          <MainSection />
-        )}/>
-        <Route exact path='/:category' component={ CategoryView } />
-        <Route exact path='/:category/:post_id' component={ PostDetail } />
-        <Route exact path='/posts/new' component={ PostNew } />
-        <Route exact path='/posts/edit' component={ PostEdit } />
+        <Switch>
+          <Route exact path='/' render={() => (
+            <MainSection />
+          )}/>
+          <Route exact path='/:category' component={ CategoryView } />
+          <Route exact path='/posts/new' component={ PostNew } />
+          <Route exact path='/posts/edit' component={ PostEdit } />
+          <Route exact path='/:category/:post_id' component={ PostDetail } />
+          <Route component={ NoMatch } />
+        </Switch>
       </div>
     )
   }
