616fd2a76b8163b8820870aef7dc24ecbd7f7aae
Laszlo Suslecz <drsuslac@gmail.com>
2017-09-18 07:52:46 +0200

    refactor: refactor api methods

--- frontend/src/api_methods/index.js
+++ frontend/src/api_methods/index.js
@@ -1,14 +1,13 @@
-//import axios from 'axios'
-
 const url = 'http://localhost:3001'
 
 const token = 'laszlo'
 
 const headers = {
-  'Authorization': token
+  'Accept': 'application/json',
+  'Authorization': token,
+  'Content-Type': 'application/json'
 }
 
 export const getPosts = () => 
   fetch(`${url}/posts`, { headers })
     .then(res => res.json())
-  
