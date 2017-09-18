71e0d2e49f918e40f27420752b775555e95b906e
Laszlo Suslecz <drsuslac@gmail.com>
2017-09-18 18:58:18 +0200

    feat: finish the form with validation for new posts. TODO: handle submit

--- frontend/src/components/PostNew.js
+++ frontend/src/components/PostNew.js
@@ -3,6 +3,36 @@ import { Link } from 'react-router-dom'
 import { Field, reduxForm } from 'redux-form'
 
 class PostNew extends Component {
+
+  renderInput(field) {
+    return (
+      <div>
+        <label>{ field.label }</label>
+        <input { ...field.input } placeholder={ field.placeholder } type='text' />
+        {field.meta.error}
+      </div>
+    )
+  }
+
+  renderSelect(field) {
+    return (
+      <div>
+        <label>{ field.label }</label>
+        <select { ...field.input } value={ field.value } />
+      </div>
+    )
+  }
+
+  renderTextarea(field) {
+    return (
+      <div>
+        <label>{ field.label }</label>
+        <textarea { ...field.input } placeholder={ field.placeholder } type='text' />
+        {field.meta.error}
+      </div>
+    )
+  }
+
   render() {
     return ( 
       <div>
@@ -14,19 +44,19 @@ class PostNew extends Component {
         <h1>create a new post</h1>
         <form>
           <Field
-            name="title" component='input' type='text' placeholder='title'
+            name="title" component={ this.renderInput } label='title' type='text' placeholder='title'
           />
           <Field
-            name="category" component='select'>
+            name="category" component='select' label='category'>
             <option value='react'>React</option>
             <option value='redux'>Redux</option>
             <option value='udacity'>Udacity</option>
           </Field>
           <Field
-            name="body" component='textarea' type='text' placeholder='content'
+            name="body" component={ this.renderTextarea } label='content' placeholder='content'
           />
           <Field
-            name="author" component='input' type='text' placeholder='name'
+            name="author" component={ this.renderInput } label='author' placeholder='name'
           />
           <button type="submit">Submit</button>
         </form>
@@ -35,6 +65,26 @@ class PostNew extends Component {
   }
 }
 
+function validate(values) {
+  const errors = {}
+
+  if(!values.title) {
+    errors.title = 'Please enter a title'
+  } 
+ /* if(!values.category) {
+    errors.category = 'Please choose a category'
+  } */
+  if(!values.body) {
+    errors.category = 'Please enter post content'
+  }
+  if(!values.author) {
+    errors.author = 'Please enter your name'
+  }
+
+  return errors
+}
+
 export default reduxForm({
+  validate,
   form: 'new-post'
 })(PostNew)
